a66220f60e825615620f147bb2d4d0a234469f09
Syed Raza Haider <97038058+syed-raza-haider@users.noreply.github.com>
2023-03-21 19:44:08 +0500

    Add product controller tests

---
+++ src/test/java/com/raza/ecommerce/controller/ProductControllerTest.java
@@ -0,0 +1,50 @@
+package com.raza.ecommerce.controller;
+
+import com.raza.ecommerce.entity.Product;
+import com.raza.ecommerce.service.ProductService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ProductController.class)
+public class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ProductService productService;
+
+    @Test
+    public void givenProducts_whenGetProducts_thenReturnJsonArray() throws Exception{
+        final List<Product> testProducts = new ArrayList<>(Arrays.asList(
+                new Product(1L, "test-product-1", "test description-1",
+                        Timestamp.valueOf(LocalDateTime.now()), Timestamp.valueOf(LocalDateTime.now()), Timestamp.valueOf(LocalDateTime.now())),
+                new Product(2L, "test-product-2", "test description-2",
+                        Timestamp.valueOf(LocalDateTime.now()), Timestamp.valueOf(LocalDateTime.now()), Timestamp.valueOf(LocalDateTime.now()))));
+
+        when(productService.fetchAll()).thenReturn(testProducts);
+
+        mockMvc.perform(get("/api/v1/product/all")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)));
+    }
+}
